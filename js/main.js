2317cde8a378998ca233afd840e35becea570978
future23365 <liusongbai2072@qq.com>
2020-05-18 18:05:41 +0800

    轻度适配手机端

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// 动态添加待办
+//基本element获取
 let inputText = document.querySelector(".heard .heard-main .inputText input")
 let ul_ongoing = document.querySelector(".main .ongoing ul");
 let ul_done = document.querySelector(".main .done ul");
@@ -7,6 +7,7 @@ let doneText = document.querySelector(".main .done-text .count");
 let text_going = document.querySelector(".main .ongoing-text");
 let text_done = document.querySelector(".main .done-text");
 let dropData = null;
+
 updateLi(ul_ongoing, ul_done);
 allowDrop(text_going);
 allowDrop(text_done);
@@ -30,6 +31,7 @@ text_done.addEventListener("drop", function(event) {
   localStorage.setItem("todo", JSON.stringify(dataJson));
   updateLi(ul_ongoing, ul_done);
 })
+
 let placeholderIndex = 0;
 let text = "";
 let showText = setInterval(function() {
@@ -47,6 +49,7 @@ inputText.addEventListener("keydown", function(event) {
     setLi();
   }
 })
+//添加函数
 function setLi() {
   let text = inputText.value;
   let dataJson = JSON.parse(localStorage.getItem("todo"));
@@ -54,6 +57,7 @@ function setLi() {
   localStorage.setItem("todo", JSON.stringify(dataJson));
   updateLi(ul_ongoing, ul_done)
 }
+//页面组件更新函数
 function updateLi(element, ul_done) {
   element.innerHTML = "";
   ul_done.innerHTML = "";
@@ -125,6 +129,7 @@ function updateLi(element, ul_done) {
   }
 }
 }
+//取消默认拖动事件
 function allowDrop(element) {
   element.addEventListener("dragenter", function(event) {
     event.preventDefault();
@@ -133,17 +138,18 @@ function allowDrop(element) {
     event.preventDefault();
   })
 }
+//轮播图
 let span = document.querySelectorAll(".heard .heard-main .logo div span");
 let div = document.querySelector(".heard .heard-main .logo div");
 let leftButton = document.querySelector(".heard .heard-main .logo .left");
 let rightButton = document.querySelector(".heard .heard-main .logo .right");
 let spanClone = span[0].cloneNode(true);
 div.appendChild(spanClone);
-
-
-
 let x = 0;
 let throllte = true;
+let logo = document.querySelector(".logo");
+let logo_width = logo.offsetWidth;
+//轮播图模拟右按钮点击
 rightButton.addEventListener("click", function() {
   if(throllte === true) {
     throllte = false;
@@ -153,7 +159,7 @@ rightButton.addEventListener("click", function() {
     x = 0;
     x++;
   }
-  animate(div, -320 * x, function() {
+  animate(div, -logo_width * x, function() {
     throllte = true;
   });
   }
@@ -163,19 +169,20 @@ leftButton.addEventListener("click", function() {
     throllte = false;
   x--;
   if(x < 0) {
-    div.style.left = (div.childNodes.length -1) * -320 + "px";
+    div.style.left = (div.childNodes.length -1) * -logo_width + "px";
     x = div.childNodes.length - 1;
     x--;
   }
-  animate(div, -320 * x,function() {
+  animate(div, -logo_width * x,function() {
     throllte = true;
   });
   }
 })
-
+//录播图计时器
 setInterval(function() {
   rightButton.click();
 }, 3000);
+//缓动动画函数
 function animate(object, target, callback) {
   object.timer = setInterval(function() {
     let step =(target - object.offsetLeft) / 10;
@@ -188,6 +195,8 @@ function animate(object, target, callback) {
   }, 50)
  
 }
+
+//显示天气功能
 let weater = document.querySelector(".main .ongoing-text .weater");
 let svg = document.querySelector(".main .ongoing-text .weater svg");
 let p = document.querySelector(".main .ongoing-text .weater p");
@@ -204,6 +213,7 @@ let weater_icon = {
   qing : "#icon-qing",
   yun_night : "#icon-ziyuan"
 }
+//天气获取
 let requestWeater = function() {
   let xhr = new XMLHttpRequest();
   xhr.open("get", "https://tianqiapi.com/free/day?appid=92968218&appsecret=DdBt3gIQ&cityid=101010100", true);
@@ -211,7 +221,7 @@ let requestWeater = function() {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       if(xhr.status >= 200 &&xhr.status < 300 || xhr.status === 304) {
-        weater.style.display = "block";
+        // weater.style.display = "block";
         let data = JSON.parse(xhr.responseText);
         let wea_img = data.wea_img;
         if(data.wea_img === "yun")
@@ -230,15 +240,16 @@ let requestWeater = function() {
 }
 
 requestWeater();
+//天气获取计时器
 setInterval(function() {
   requestWeater();
 }, 600000)
-
+//每日一句功能
 let quotation = document.querySelector(".main .ongoing-text .quotation ");
 window.addEventListener("load", function() {
-  quotation.style.left = 20 + 'px';
+  quotation.style.left = "-2rem";
 })
-
+//每日一句获取函数
 let requsetQuoation = function() {
 let xhr = new XMLHttpRequest();
 xhr.open("post", "https://api.66mz8.com/api/quotation.php?format=json", true);
@@ -247,17 +258,20 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState === 4) {
     if(xhr.status >= 200 &&xhr.status < 300 || xhr.status === 304) {
       let data = JSON.parse(xhr.responseText);
-      quotation.style.left = -40 + 'px';
+      quotation.style.left = "-6rem";
       setTimeout(function() {
+        if(data.quotation.length > 25) {
+          data.quotation = "不要等每一盏灯都熄灭才期盼光明"
+        }
         quotation.innerHTML = data.quotation;
         console.log(data.quotation);
-      quotation.style.left = 20 + 'px';
+        quotation.style.left = "-2rem";
       }, 1000)
-        
     }
   }
 }
 }
+//更新每日一句计时器
 setInterval(function() {
   requsetQuoation();
 }, 30000)
